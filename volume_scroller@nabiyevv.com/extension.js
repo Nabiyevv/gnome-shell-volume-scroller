558ea8eebbd061eacdafc219b49bee72570c78a1
Nabiyevv <maqa.nabiev@gmail.com>
2025-04-16 01:05:42 +0400

    refactor

--- volume_scroller@nabiyevv.com/extension.js
+++ volume_scroller@nabiyevv.com/extension.js
@@ -1,47 +1,53 @@
-import Clutter from 'gi://Clutter';
-import Gio from 'gi://Gio';
-import * as Main from 'resource:///org/gnome/shell/ui/main.js';
-import * as Volume from 'resource:///org/gnome/shell/ui/status/volume.js';
+import Clutter from "gi://Clutter";
+import Gio from "gi://Gio";
+import * as Main from "resource:///org/gnome/shell/ui/main.js";
+import * as Volume from "resource:///org/gnome/shell/ui/status/volume.js";
 
 const VolumeScrollerIcons = [
-    'audio-volume-muted-symbolic',
-    'audio-volume-low-symbolic',
-    'audio-volume-medium-symbolic',
-    'audio-volume-high-symbolic'
+    "audio-volume-muted-symbolic",
+    "audio-volume-low-symbolic",
+    "audio-volume-medium-symbolic",
+    "audio-volume-high-symbolic",
 ];
 
 export default class VolumeScroller {
     constructor() {
-       this.controller = Volume.getMixerControl();
-       this.panel = Main.panel;
+        this.controller = Volume.getMixerControl();
+        this.panel = Main.panel;
 
-       this.enabled = false;
-       this.sink = null;
-       
-       this.volume_max = this.controller.get_vol_max_norm();
-       
-       this.settings = this._get_settings();
-       this.volume_step = this.settings.get_double('volume-step') * this.volume_max;
-       this.inverse_scrolling = this.settings.get_boolean('inverse-scrolling');
-       this.horizontal_scrolling = this.settings.get_boolean('horizontal-scrolling');
-       
-       this.scroll_binding = null;
-       this.sink_binding = null;
-       this.settings_binding = null;
-       this.direction_binding = null;
+        this.enabled = false;
+        this.sink = null;
+
+        this.volume_max = this.controller.get_vol_max_norm();
+
+        this.settings = this._get_settings();
+        this.volume_step =
+            this.settings.get_double("volume-step") * this.volume_max;
+        this.inverse_scrolling = this.settings.get_boolean("inverse-scrolling");
+        this.horizontal_scrolling = this.settings.get_boolean(
+            "horizontal-scrolling",
+        );
+
+        this.scroll_binding = null;
+        this.sink_binding = null;
+        this.settings_binding = null;
+        this.direction_binding = null;
     }
 
     _get_settings() {
         let dir = import.meta.url.slice(7); // Remove 'file://' prefix
-        dir = dir.slice(0, dir.lastIndexOf('/'));
+        dir = dir.slice(0, dir.lastIndexOf("/"));
 
         let source = Gio.SettingsSchemaSource.new_from_directory(
-            dir + '/schemas',
+            dir + "/schemas",
             Gio.SettingsSchemaSource.get_default(),
-            false
+            false,
         );
 
-        let schema = source.lookup('org.gnome.shell.extensions.volume-scroller', true);
+        let schema = source.lookup(
+            "org.gnome.shell.extensions.volume-scroller",
+            true,
+        );
         return new Gio.Settings({ settings_schema: schema });
     }
 
@@ -54,28 +60,34 @@ export default class VolumeScroller {
         this.sink = this.controller.get_default_sink();
 
         this.scroll_binding = this.panel.connect(
-            'scroll-event',
-            (actor, event) => this._handle_scroll(actor, event));
+            "scroll-event",
+            (actor, event) => this._handle_scroll(actor, event),
+        );
 
         this.sink_binding = this.controller.connect(
-            'default-sink-changed',
-            (controller, id) => this._handle_sink_change(controller, id));
-            
+            "default-sink-changed",
+            (controller, id) => this._handle_sink_change(controller, id),
+        );
+
         this.settings_binding = this.settings.connect(
-            'changed::volume-step',
-            () => this._handle_settings_change());
+            "changed::volume-step",
+            () => this._handle_settings_change(),
+        );
 
         this.direction_binding = this.settings.connect(
-            'changed::inverse-scrolling',
-            () => this._handle_direction_change());
-        
+            "changed::inverse-scrolling",
+            () => this._handle_direction_change(),
+        );
+
         this.middle_click_binding = this.panel.connect(
-            'button-press-event',
-            (actor, event) => this._handle_middle_click(actor, event));
+            "button-press-event",
+            (actor, event) => this._handle_middle_click(actor, event),
+        );
 
         this.horizontal_change_binding = this.settings.connect(
-            'changed::horizontal-scrolling',
-            () => this._handle_horizontal_change());
+            "changed::horizontal-scrolling",
+            () => this._handle_horizontal_change(),
+        );
     }
 
     disable() {
@@ -96,52 +108,61 @@ export default class VolumeScroller {
                 this.settings.disconnect(this.direction_binding);
                 this.direction_binding = null;
             }
-            
+
             this.panel.disconnect(this.middle_click_binding);
             this.middle_click_binding = null;
         }
     }
 
     _handle_settings_change() {
-        this.volume_step = this.settings.get_double('volume-step') * this.volume_max;
+        this.volume_step =
+            this.settings.get_double("volume-step") * this.volume_max;
     }
 
     _handle_direction_change() {
-        this.inverse_scrolling = this.settings.get_boolean('inverse-scrolling');
+        this.inverse_scrolling = this.settings.get_boolean("inverse-scrolling");
     }
-    
+
     _handle_horizontal_change() {
-        this.horizontal_scrolling = this.settings.get_boolean('horizontal-scrolling');
+        this.horizontal_scrolling = this.settings.get_boolean(
+            "horizontal-scrolling",
+        );
     }
 
     _handle_scroll(actor, event) {
         let volume = this.sink.volume;
         let direction = event.get_scroll_direction();
-        
-        if(this.inverse_scrolling) {
-            if(this.horizontal_scrolling) {
-                if (direction === Clutter.ScrollDirection.LEFT) volume -= this.volume_step;
-                else if (direction === Clutter.ScrollDirection.RIGHT) volume += this.volume_step;
+
+        if (this.inverse_scrolling) {
+            if (this.horizontal_scrolling) {
+                if (direction === Clutter.ScrollDirection.LEFT)
+                    volume -= this.volume_step;
+                else if (direction === Clutter.ScrollDirection.RIGHT)
+                    volume += this.volume_step;
                 else return Clutter.EVENT_PROPAGATE;
-            }
-            else {
-                if (direction === Clutter.ScrollDirection.UP) volume += this.volume_step;
-                else if (direction === Clutter.ScrollDirection.DOWN) volume -= this.volume_step;
+            } else {
+                if (direction === Clutter.ScrollDirection.UP)
+                    volume += this.volume_step;
+                else if (direction === Clutter.ScrollDirection.DOWN)
+                    volume -= this.volume_step;
                 else return Clutter.EVENT_PROPAGATE;
             }
         } else {
-            if(this.horizontal_scrolling) {
-                if (direction === Clutter.ScrollDirection.LEFT) volume += this.volume_step;
-                else if (direction === Clutter.ScrollDirection.RIGHT) volume -= this.volume_step;
-                    else return Clutter.EVENT_PROPAGATE;
-            }
-            else {
-                if (direction === Clutter.ScrollDirection.UP) volume -= this.volume_step;
-                else if (direction === Clutter.ScrollDirection.DOWN) volume += this.volume_step;
-                    else return Clutter.EVENT_PROPAGATE;
+            if (this.horizontal_scrolling) {
+                if (direction === Clutter.ScrollDirection.LEFT)
+                    volume += this.volume_step;
+                else if (direction === Clutter.ScrollDirection.RIGHT)
+                    volume -= this.volume_step;
+                else return Clutter.EVENT_PROPAGATE;
+            } else {
+                if (direction === Clutter.ScrollDirection.UP)
+                    volume -= this.volume_step;
+                else if (direction === Clutter.ScrollDirection.DOWN)
+                    volume += this.volume_step;
+                else return Clutter.EVENT_PROPAGATE;
             }
         }
-    
+
         volume = Math.min(volume, this.volume_max);
         volume = Math.max(volume, 0);
 
@@ -156,22 +177,20 @@ export default class VolumeScroller {
     _handle_sink_change(controller, id) {
         this.sink = controller.lookup_stream_id(id);
     }
-    
+
     _handle_middle_click(actor, event) {
         const button = event.get_button();
         if (button === Clutter.BUTTON_MIDDLE) {
             let isMuted = this.sink.get_is_muted();
             isMuted = !isMuted;
-            
+
             this.sink.change_is_muted(isMuted);
-            
-            if (isMuted)
-                this._show_volume(0);
-            else 
-                this._show_volume(this.sink.volume);
-            
+
+            if (isMuted) this._show_volume(0);
+            else this._show_volume(this.sink.volume);
+
             return Clutter.EVENT_STOP;
-        } 
+        }
         return Clutter.EVENT_PROPAGATE;
     }
 
--- volume_scroller@nabiyevv.com/prefs.js
+++ volume_scroller@nabiyevv.com/prefs.js
@@ -1,7 +1,7 @@
-import Adw from 'gi://Adw';
-import Gtk from 'gi://Gtk';
-import Gio from 'gi://Gio';
-import { ExtensionPreferences } from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
+import Adw from "gi://Adw";
+import Gtk from "gi://Gtk";
+import Gio from "gi://Gio";
+import { ExtensionPreferences } from "resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js";
 
 export default class VolumeScrollerPreferences extends ExtensionPreferences {
     fillPreferencesWindow(window) {
@@ -14,13 +14,13 @@ export default class VolumeScrollerPreferences extends ExtensionPreferences {
 
         // Create a row for volume step
         const volumeRow = new Adw.ActionRow({
-            title: 'Volume Change Step',
-            subtitle: 'The amount to change volume when scrolling (1-20%)',
+            title: "Volume Change Step",
+            subtitle: "The amount to change volume when scrolling (1-20%)",
         });
 
         // Create the volume step adjustment
         const adjustment = new Gtk.Adjustment({
-            lower: 1,  // Show as percentage (1-20 instead of 0.01-0.2)
+            lower: 1, // Show as percentage (1-20 instead of 0.01-0.2)
             upper: 20,
             step_increment: 1,
         });
@@ -35,9 +35,9 @@ export default class VolumeScrollerPreferences extends ExtensionPreferences {
         });
 
         // Set initial value and handle changes
-        scale.set_value(settings.get_double('volume-step') * 100);
-        scale.connect('value-changed', (widget) => {
-            settings.set_double('volume-step', widget.get_value() / 100);
+        scale.set_value(settings.get_double("volume-step") * 100);
+        scale.connect("value-changed", (widget) => {
+            settings.set_double("volume-step", widget.get_value() / 100);
         });
 
         // Add scale to the row
@@ -47,21 +47,22 @@ export default class VolumeScrollerPreferences extends ExtensionPreferences {
 
         // Create a row for scroll direction toggle
         const directionRow = new Adw.ActionRow({
-            title: 'Inverse Scroll Direction',
-            subtitle: 'When enabled, scroll up decreases volume and scroll down increases volume',
+            title: "Inverse Scroll Direction",
+            subtitle:
+                "When enabled, scroll up decreases volume and scroll down increases volume",
         });
 
         const toggle = new Gtk.Switch({
-            active: settings.get_boolean('inverse-scrolling'),
+            active: settings.get_boolean("inverse-scrolling"),
             valign: Gtk.Align.CENTER,
         });
 
         // Bind the toggle to the settings
         settings.bind(
-            'inverse-scrolling',
+            "inverse-scrolling",
             toggle,
-            'active',
-            Gio.SettingsBindFlags.DEFAULT
+            "active",
+            Gio.SettingsBindFlags.DEFAULT,
         );
 
         // Add toggle to the row
@@ -69,25 +70,25 @@ export default class VolumeScrollerPreferences extends ExtensionPreferences {
         directionRow.set_activatable(true);
         directionRow.set_activatable_widget(toggle);
         group.add(directionRow);
-        
-        
+
         // Create a row for horizontal scrolling toggle
         const horizontalRow = new Adw.ActionRow({
-            title: 'Enable Horizontal Scrolling',
-            subtitle: 'When enabled, allows horizontal scrolling to change volume',
+            title: "Enable Horizontal Scrolling",
+            subtitle:
+                "When enabled, allows horizontal scrolling to change volume",
         });
 
         const horizontalToggle = new Gtk.Switch({
-            active: settings.get_boolean('horizontal-scrolling'),
+            active: settings.get_boolean("horizontal-scrolling"),
             valign: Gtk.Align.CENTER,
         });
 
         // Bind the toggle to the settings
         settings.bind(
-            'horizontal-scrolling',
+            "horizontal-scrolling",
             horizontalToggle,
-            'active',
-            Gio.SettingsBindFlags.DEFAULT
+            "active",
+            Gio.SettingsBindFlags.DEFAULT,
         );
 
         // Add toggle to the row
